b96fe116f5a3c98db0b5c0495f2422902acfea2c
Eduardo <e_rodriguez13@mail.fhsu.edu>
2023-06-26 22:53:37 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,58 @@
+// Question 1
+//The variable myNumber should by defined.
+var myNumber;
+
+/* Question 2
+The variable myNumber should be type number.
+The variable myNumber should be equal to 5.*/
+myNumber = 5;
+
+/* Question 3
+The variable myOtherNumber should by defined.
+The variable myOtherNumber should be type number.
+The variable myOtherNumber should be equal to 5.*/
+var myOtherNumber ;
+myOtherNumber = 5
+
+/* Question 4
+The variable myTotal should by defined.
+The variable myTotal should be type number.
+The variable myTotal should be equal to the product of myNumber and myOtherNumber.‣*/
+var myTotal;
+myTotal= myNumber*myOtherNumber;
+
+/* Question 5
+The variable myName should by defined.
+The variable myName should be type string.
+The variable myCombo should by defined.
+The variable myCombo should be type string.
+The variable myCombo should be equal to the sum of myNumber and myOtherNumber.‣
+*/
+var myName = "Eduardo";
+var myCombo = (myNumber+myOtherNumber).toString();
+
+/* Question 6
+The variable myOtherCombo should by defined.
+The variable myOtherCombo should be type number.
+The variable myOtherCombo should be equal to the sum of myNumber and myOtherNumber.‣ 
+*/
+var myOtherCombo = myNumber+myOtherNumber;
+
+/* Question 7
+The variable myRemainder should by defined, of type number and equal to the remainder of myNumber divided by 5.
+*/
+var myRemainder = myNumber%5;
+
+/* Question 8
+The variabale i should be defined, of type number and equal to 2
+*/
+var i = 2;
+
+/* Question 9
+The variable int shoudl be defined, of type number, and equal to 4*/
+var int = 4;
+
+/* Question 10
+The variable myBoolean should be defined of type boolean and be false;*/
+var myBoolean = false;
+
